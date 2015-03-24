0f3f8ffd2bfc6537bef17deb9f0b9bc4271d7d51
danijelr <rajic.danijel1@gmail.com>
2015-03-24 13:42:41 +0100

    cssand new buttons

--- laravel/resources/views/items/index.blade.php
+++ laravel/resources/views/items/index.blade.php
@@ -3,14 +3,15 @@
 	<a href="{{ route('item.create') }}" class="btn btn-success">Add new item</a>
 	<div class="table-responsive"><br/>
 	<table style="width:100%" class="table-striped table-hover table" >
-	 <tr>
+	 <tr class="info">
             <th>Username</th>
             <th>Title</th>
             <th>Description</th>
+            <th>Time of creation</th>
           </tr>
 	@foreach($items as $item)
-		<tr>
-		<td>
+		<tr >
+		<td class="info">
 			{{ $item->user->name }}
 		</td>
 		
@@ -20,7 +21,14 @@
 			</p>
 		</td>
 
-			<td><p>{{ $item->description }}</p></td>
+		<td>
+			<p>
+				{{ $item->description }}
+			</p>
+		</td>
+		<td>
+			{{ $item->created_at->format('F d, Y h:ia') }} 
+		</td>
 			<td><a href="{{ route('item.edit', $item->id) }}" class="btn btn-primary">EDIT</a></td>
 			<td>{!! Form::open(['route' => ['item.destroy', $item->id], 'method' => 'DELETE']) !!}
 			{!! Form::submit('DELETE', ['class' => 'btn btn-danger']) !!}
